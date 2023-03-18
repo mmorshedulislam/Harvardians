a0e19c930bdd0a5aa43747dc7bd84e921fcfee8f
MORSHED-HOME <morshed952640@gmail.com>
2023-03-18 21:30:58 +0600

    react-photo-view implement in gallery

--- client-Harvardians/Components/Home/Gallery/Gallery.js
+++ client-Harvardians/Components/Home/Gallery/Gallery.js
@@ -1,3 +1,6 @@
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+
 const Gallery = () => {
   const imgs = [
     {
@@ -26,14 +29,18 @@ const Gallery = () => {
     },
   ];
   return (
-    <div className="max-w-7xl mx-auto p-10">
-      <h2 className="text-center text-4xl font-bold my-5">Gallery</h2>
-      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-10">
-        {imgs.map((img, i) => (
-          <img key={i} src={img?.img} />
-        ))}
+    <PhotoProvider>
+      <div className="max-w-7xl mx-auto p-10">
+        <h2 className="text-center text-4xl font-bold my-5">Gallery</h2>
+        <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-10">
+          {imgs.map((img, i) => (
+            <PhotoView key={i} src={img?.img}>
+              <img key={i} src={img?.img} />
+            </PhotoView>
+          ))}
+        </div>
       </div>
-    </div>
+    </PhotoProvider>
   );
 };
 
