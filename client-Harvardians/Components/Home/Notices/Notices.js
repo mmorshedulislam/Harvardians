79a85401b40b6dbb9388491d149a7d0091b1ebfa
MORSHED-OFFICE <morshed952640@gmail.com>
2023-03-19 18:10:13 +0600

    'update_from_shop'

--- client-Harvardians/Components/Home/Notices/Notices.js
+++ client-Harvardians/Components/Home/Notices/Notices.js
@@ -5,7 +5,9 @@ const Notices = () => {
     <div className="max-w-7xl mx-auto p-10">
       <h2 className="text-center text-5xl font-bold my-5">Notices</h2>
       <div className="grid md: grid-cols-2 lg:grid-cols-3">
-        <NoticeCard />
+        {[...Array(3)].map((notice) => (
+          <NoticeCard />
+        ))}
       </div>
     </div>
   );
